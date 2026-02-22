26d9267210be6a3f0c48cc104a2de9e729f3d9a4
tkm-sano <takuma.sano@keio.jp>
2026-02-22 14:59:18 +0900

    タイムアウト時間の修正

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,2 +1,2 @@
 window.MENU_TALENT_API_URL = "https://script.google.com/macros/s/AKfycbw-Wrb9pPzSR8syilUQZo3Z0o9seHgG1F7cZ9zXS0KZiJptVoH7GSXLZWIhn3uXCSHS/exec";
-window.MENU_TALENT_API_TIMEOUT_MS = 30000;
+window.MENU_TALENT_API_TIMEOUT_MS = 60000;
