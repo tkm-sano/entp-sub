18de5b26bddf39bcde93cbe370e1391782d15dc6
tkm-sano <takuma.sano@keio.jp>
2026-02-23 19:17:42 +0900

    パスの修正

--- .github/scripts/fetchJobs.mjs
+++ .github/scripts/fetchJobs.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { google } from 'googleapis';
 
 // サービスアカウントの JSON キーを読み込む
-const keyFile = './.github/scripts/service-account.json';
+const keyFile = './service-account.json';
 const auth = new google.auth.GoogleAuth({
   keyFile,
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
