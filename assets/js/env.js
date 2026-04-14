8ed000e30806c28dc902dcf80ea7eda381c62b28
tkm-sano <takuma.sano@keio.jp>
2026-04-14 14:42:40 +0900

    Update Apps Script apply endpoint

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,2 +1,2 @@
-window.MENU_TALENT_APPLY_API_URL = "https://script.google.com/macros/s/AKfycbz1siu5ov9HVHMM4N_QYLCz41ZpL7O4OHT_SqErd1wtKciAMYWUjj3UZLiWaJC-pd1LlA/exec";
+window.MENU_TALENT_APPLY_API_URL = "https://script.google.com/macros/s/AKfycbyjr_oNn1Rr5FTPkg_Ls-RbxUJqggicnbY5QphP_fKqJYM4tJqjoHRNj4nJNiUOya3rmA/exec";
 window.MENU_TALENT_API_TIMEOUT_MS = 60000;
